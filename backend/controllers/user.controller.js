b1330b7aff69c1def3b9965c6ba2dee2db1d3fe2
Ranajit305 <nathranajit6@gmail.com>
2025-06-21 19:13:53 +0530

    token management udpate

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -85,7 +85,11 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
     try {
-        res.clearCookie('jwt', { maxAge: 0 });
+        res.clearCookie('jwt', {
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true,
+        });
         res.status(200).json({success: true, message: 'Logged Out'})
     } catch (error) {
         res.status(500).json({success: false, message: error.message})
